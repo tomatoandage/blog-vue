85b97baa0a13cf94e7482c7abbddd748511e14e6
yinchao <3219815800@qq.com>
2024-12-02 07:51:58 +0800

    登录页开发

--- src/main.js
+++ src/main.js
@@ -3,9 +3,21 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 
+//导入路由
 import router from '@/router'
 
+//导入Animate.css (Animate.css 是一个跨浏览器的 CSS 动画库，提供了许多预设的、流畅的动画效果。)
+import 'animate.css';
+
+//导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
+
+//引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+	app.component(key, component)
+}
 
 app.use(router)
 app.mount('#app')
