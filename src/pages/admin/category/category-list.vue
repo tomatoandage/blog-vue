1ad406d53ad22845a137e732e6f06c2f5241f984
yinchao <3219815800@qq.com>
2024-12-11 09:10:52 +0800

    博客分类开发

--- src/pages/admin/category/category-list.vue
+++ src/pages/admin/category/category-list.vue
@@ -1,79 +1,123 @@
 <template>
-	<el-form :inline="true" class="demo-form-inline">
-	  <el-form-item label="分类名称">
-		<el-input v-model="formData.categoryName" placeholder="请输入分类名称"></el-input>
-	  </el-form-item>
-	  <el-form-item label="模糊查询">
-		<el-input v-model="formData.keyword" placeholder="请输入模糊查询内容"></el-input>
-	  </el-form-item>
-	  <el-form-item label="创建日期">
-		<el-date-picker
-		  v-model="formData.createDate"
-		  type="date"
-		  placeholder="选择日期">
-		</el-date-picker>
-	  </el-form-item>
-	  <el-form-item label="时间范围">
-		<el-time-picker
-		  v-model="formData.startTime"
-		  placeholder="开始时间"
-		  :picker-options="startTimeOptions">
-		</el-time-picker>
-		<el-time-picker
-		  v-model="formData.endTime"
-		  placeholder="结束时间"
-		  :picker-options="endTimeOptions"
-		  style="width: 100%;">
-		</el-time-picker>
-	  </el-form-item>
-	  <el-form-item>
-		<el-button type="primary" @click="handleSearch">查询</el-button>
-		<el-button @click="handleReset">重置</el-button>
-	  </el-form-item>
-	</el-form>
-  </template>
+	<div>
+		<el-card>
+			<el-form :inline="true" class="demo-form-inline">
+				<el-form-item label="分类名称">
+					<el-input v-model="category.name" placeholder="请输入分类名称"></el-input>
+				</el-form-item>
+				<el-form-item label="创建日期">
+					<div class="demo-date-picker">
+						<div class="block">
+							<el-date-picker v-model="value2" type="daterange" unlink-panels range-separator="To"
+								start-placeholder="开始时间" end-placeholder="结束时间" :shortcuts="shortcuts" :size="size" />
+						</div>
+					</div>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="handleSearch">查询</el-button>
+					<el-button @click="handleReset">重置</el-button>
+				</el-form-item>
+			</el-form>
+		</el-card>
+		<el-card>
+			<el-table :data="tableData" border style="width: 100%">
+				<el-table-column prop="date" label="Date" width="180" />
+				<el-table-column prop="name" label="Name" width="180" />
+				<el-table-column prop="address" label="Address" />
+			</el-table>
+			<el-pagination v-model:current-page="currentPage4" v-model:page-size="pageSize4" :page-sizes="[100, 200, 300, 400]"
+				:size="size" :disabled="disabled" :background="background" layout="total, sizes, prev, pager, next, jumper"
+				:total="400" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+		</el-card>
+	</div>
+</template>
   
-  <script>
-  export default {
-	data() {
-	  return {
-		formData: {
-		  categoryName: '',
-		  keyword: '',
-		  createDate: '',
-		  startTime: '',
-		  endTime: ''
+<script setup>
+import { ref } from 'vue'
+
+const currentPage4 = ref(4)
+const pageSize4 = ref(100)
+const size = ref('default')
+const background = ref(false)
+const disabled = ref(false)
+
+const handleSizeChange = (val) => {
+	console.log(`${val} items per page`)
+}
+const handleCurrentChange = (val) => {
+	console.log(`current page: ${val}`)
+}
+
+const category = {
+	name: '',
+	startTime: '',
+	endTime: ''
+}
+
+const shortcuts = ref([
+	{
+		text: 'Last week',
+		value: () => {
+			const end = new Date()
+			const start = new Date()
+			start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+			return [start, end]
 		},
-		startTimeOptions: {
-		  selectableRange: '18:30:00 - 20:30:00' // 限制时间选择范围，这里只是个示例
-		},
-		endTimeOptions: {
-		  selectableRange: '18:30:00 - 20:30:00' // 同上，实际应用中可能需要根据需求调整
-		}
-	  }
 	},
-	methods: {
-	  handleSearch() {
-		// 在这里处理搜索逻辑，例如发送请求到后端
-		console.log(this.formData)
-	  },
-	  handleReset() {
-		// 重置表单数据
-		this.formData = {
-		  categoryName: '',
-		  keyword: '',
-		  createDate: '',
-		  startTime: '',
-		  endTime: ''
-		}
-	  }
-	}
-  }
-  </script>
+	{
+		text: 'Last month',
+		value: () => {
+			const end = new Date()
+			const start = new Date()
+			start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+			return [start, end]
+		},
+	},
+	{
+		text: 'Last 3 months',
+		value: () => {
+			const end = new Date()
+			const start = new Date()
+			start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+			return [start, end]
+		},
+	},
+])
+
+const tableData = [
+	{
+		date: '2016-05-03',
+		name: 'Tom',
+		address: 'No. 189, Grove St, Los Angeles',
+	},
+	{
+		date: '2016-05-02',
+		name: 'Tom',
+		address: 'No. 189, Grove St, Los Angeles',
+	},
+	{
+		date: '2016-05-04',
+		name: 'Tom',
+		address: 'No. 189, Grove St, Los Angeles',
+	},
+	{
+		date: '2016-05-01',
+		name: 'Tom',
+		address: 'No. 189, Grove St, Los Angeles',
+	},
+]
+</script>
   
-  <style scoped>
-  .demo-form-inline {
-	max-width: 600px;
+<style scoped>
+.demo-form-inline {
 	margin: 0 auto;
-  }
-  </style>
+}
+
+.search-dev {
+	padding: auto;
+}
+
+.el-card.is-always-shadow {
+	margin: 10px;
+}
+</style>
